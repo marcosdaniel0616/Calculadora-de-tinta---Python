9e19dd7fcd1f40f85ec0ef9f66b935db41c94c64
Marcos Daniel <marcos.daniel0616@gmail.com>
2021-05-28 14:05:54 -0300

    Aula 3 finalizada - Captação de Inputs do usuário!

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-largura = 5
-altura = 2.9
-profundidade = 3
+"""
+nome_da_variável: float -> o nome disso é typehint, é uma "sugestão" sobre qual o tipo da variável
+"""
+
+largura: float = float(input('Qual a largura do cômodo? '))
+profundidade: float = float(input('Qual a profundidade do cômodo? '))
+altura: float = 2.9  # 2.9 é o tamanho padrão de altura das construções do Brasil
 
 
-print(2 * (largura + profundidade) * altura)
-
-print(largura * profundidade)
+print("a área das paredes é",  (2 * (largura + profundidade) * altura))
