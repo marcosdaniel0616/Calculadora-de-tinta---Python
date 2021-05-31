c8016435fd75e6e6c34753acc5fa6b8cd901fa4d
Marcos Daniel <marcos.daniel0616@gmail.com>
2021-05-31 13:37:07 -0300

    Aula 5 - Orientação a objetos finalizada!

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+from calculadora import Calculadora
+
+calc = Calculadora()
+
 largura: float = float(input('Qual a largura do cômodo? '))
 profundidade: float = float(input('Qual a profundidade do cômodo? '))
 altura: float = 2.9  # 2.9 é o tamanho padrão de altura das construções do Brasil
 
-area_paredes: float = (2 * (largura + profundidade) * altura)
+calc.area_paredes = (2 * (largura + profundidade) * altura)
 
-print("a área das paredes é",  area_paredes)
+print("a área das paredes é",  calc.area_paredes)
 
-area_teto: float = largura * profundidade
+calc.area_teto = largura * profundidade
 
 print(
     'A área do teto é:',
-    area_teto
+    calc.area_teto
 )
 
 print(
     'A litragem de tinta é necessária:',
-    ((area_paredes + area_teto) / 10)
+    ((calc.area_paredes + calc.area_teto) / 10)
 )
