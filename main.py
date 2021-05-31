56ac03aa904fd12b9a99ba00d24a670cc9b5f6b1
Marcos Daniel <marcos.daniel0616@gmail.com>
2021-05-31 13:53:15 -0300

    Aula 6 - Criação de métodos finalizada!

--- main.py
+++ main.py
@@ -6,18 +6,19 @@ largura: float = float(input('Qual a largura do cômodo? '))
 profundidade: float = float(input('Qual a profundidade do cômodo? '))
 altura: float = 2.9  # 2.9 é o tamanho padrão de altura das construções do Brasil
 
-calc.area_paredes = (2 * (largura + profundidade) * altura)
+# calc.area_paredes = (2 * (largura + profundidade) * altura)
 
-print("a área das paredes é",  calc.area_paredes)
+print("a área das paredes é",
+      calc.calcular_area_paredes(largura, profundidade, altura))
 
-calc.area_teto = largura * profundidade
+# calc.area_teto = largura * profundidade
 
 print(
     'A área do teto é:',
-    calc.area_teto
+    calc.calcular_area_teto(largura, profundidade)
 )
 
 print(
     'A litragem de tinta é necessária:',
-    ((calc.area_paredes + calc.area_teto) / 10)
+    (calc.calcular_litragem_necessaria())
 )
