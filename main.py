8aeed1e77575f83aa2cd5f2428ae010408839760
Marcos Daniel <marcos.daniel0616@gmail.com>
2021-05-31 13:31:56 -0300

    Removi o comentário sobre o type hint

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-"""
-nome_da_variável: float -> o nome disso é typehint, é uma "sugestão" sobre qual o tipo da variável
-
-Type hint no Python:
-Type Hint é um mecanismo do Python 3 através do qual podemos dar uma dica
-sobre o tipo de uma variável.
-
-area_parede : float = 2.5
-Embora seja possível com esse recurso alertar o leitor do código
-sobre o tipo de dado que esperamos que a variável receba,
-Type Hint não altera o modelo de tipagem do Python, que é dinâmico.
-"""
-
 largura: float = float(input('Qual a largura do cômodo? '))
 profundidade: float = float(input('Qual a profundidade do cômodo? '))
 altura: float = 2.9  # 2.9 é o tamanho padrão de altura das construções do Brasil
