b836a3d85196d49ba8a8b03804641bff4fb58ea7
Marcos Daniel <marcos.daniel0616@gmail.com>
2021-05-31 14:02:29 -0300

    Aula 7 - Construtor da classe finalizada!

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 from calculadora import Calculadora
+from comodo import Comodo
 
 calc = Calculadora()
 
-largura: float = float(input('Qual a largura do cômodo? '))
-profundidade: float = float(input('Qual a profundidade do cômodo? '))
-altura: float = 2.9  # 2.9 é o tamanho padrão de altura das construções do Brasil
-
-# calc.area_paredes = (2 * (largura + profundidade) * altura)
+comodo = Comodo(
+    largura=input('Qual a largura do cômodo? '),
+    profundidade=input('Qual a profundidade do cômodo? ')
+)
 
 print("a área das paredes é",
-      calc.calcular_area_paredes(largura, profundidade, altura))
-
-# calc.area_teto = largura * profundidade
+      calc.calcular_area_paredes(comodo.largura, comodo.profundidade, comodo.altura))
 
 print(
     'A área do teto é:',
-    calc.calcular_area_teto(largura, profundidade)
+    calc.calcular_area_teto(comodo.largura, comodo.profundidade)
 )
 
 print(
